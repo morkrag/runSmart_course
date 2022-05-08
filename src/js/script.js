897eace6d45ae24e2e2610c756f9bb773ae33da6
morkrag <73593430+morkrag@users.noreply.github.com>
2022-05-08 19:57:48 +0300

    Remote repository and local project synchronisation commit

--- src/js/script.js
+++ src/js/script.js
@@ -40,6 +40,68 @@ document.querySelector('.next').onclick = function () {
     toggleSlide('.catalog-item__back');
   });
 
+  // Modal
+
+  $('[data-modal=consultation]').on('click', function(){
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+  });
+  $('.button_mini').on('click', function(){
+    $('.overlay, #order').fadeIn('slow');
+  });
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    });
+  });
+
+  $('form').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer.smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  //Smooth scroll and pageup
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $(".pageup").fadeIn();
+    } else {
+      $(".pageup").fadeOut();
+    }
+  });
+
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 })(jQuery);
 
 
